72640f808f291abf161c7124c005cd465eafdbeb
Jonathan van Wersch <jonathanvwersch@gmail.com>
2021-07-09 18:20:15 +0100

    fix certificate

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,7 +1,9 @@
-import knex from 'knex';
-const knexfile = require('../../knexfile.js');
+import knex from "knex";
+const knexfile = require("../../knexfile.js");
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
+if (process.env.NODE_ENV === "development") {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+}
 const db = knex(knexfile[env]);
-
 export default db;
