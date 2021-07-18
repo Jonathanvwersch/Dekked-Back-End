3782a4d5ae26d6597032e6a829abdc3ee2394d1d
Jonathan van Wersch <jonathanvwersch@gmail.com>
2021-07-18 20:50:13 +0100

    Update flashcard with learning status

--- src/Controllers/FlashcardController.ts
+++ src/Controllers/FlashcardController.ts
@@ -143,6 +143,7 @@ export class FlashcardController {
             status,
             id,
             owner_id,
+            learning_status,
           });
 
           const fullFlashcard: FlashcardInterface = {
--- src/utils/spaced-repetition/spacedRepetition.ts
+++ src/utils/spaced-repetition/spacedRepetition.ts
@@ -12,24 +12,32 @@ export const spacedRepetition = (
   learning_status: FlashcardLearningStatus
 ) => {
   currentFlashcard.interval = interval;
-  currentFlashcard.learning_status = learning_status;
-
   const today = new Date();
 
   const nextDueDate = new Date(
-    currentFlashcard.due_date?.setUTCDate(today.getUTCDate() + interval)
+    (new Date(currentFlashcard.due_date) || today)?.setUTCDate(
+      today.getUTCDate() + interval
+    )
   );
 
-  if (currentFlashcard?.status === FlashcardStatus.NEW) {
-    currentFlashcard.due_date = today;
+  if (
+    currentFlashcard.status === FlashcardStatus.NEW ||
+    currentFlashcard.status === FlashcardStatus.NEW_1 ||
+    currentFlashcard.status === FlashcardStatus.NEW_2
+  ) {
     if (quality === FlashcardQuality.REPEAT) {
+      currentFlashcard.due_date = today;
       currentFlashcard.failed_consecutive_attempts += 1;
       currentFlashcard.interval = 0;
-      currentFlashcard.due_date = today;
-    } else {
-      if (currentFlashcard?.status === FlashcardStatus.NEW) {
+      if (currentFlashcard.status === FlashcardStatus.NEW_1) {
+        currentFlashcard.status = FlashcardStatus.NEW;
+      } else if (currentFlashcard.status === FlashcardStatus.NEW_2) {
         currentFlashcard.status = FlashcardStatus.NEW_1;
-      } else if (currentFlashcard?.status === FlashcardStatus.NEW_1) {
+      }
+    } else {
+      if (currentFlashcard.status === FlashcardStatus.NEW) {
+        currentFlashcard.status = FlashcardStatus.NEW_1;
+      } else if (currentFlashcard.status === FlashcardStatus.NEW_1) {
         currentFlashcard.status = FlashcardStatus.NEW_2;
       } else {
         currentFlashcard.status = FlashcardStatus.GRADUATED;
@@ -38,7 +46,7 @@ export const spacedRepetition = (
       currentFlashcard.failed_consecutive_attempts = 0;
       currentFlashcard.due_date = nextDueDate;
     }
-  } else if (currentFlashcard?.status === FlashcardStatus.GRADUATED) {
+  } else if (currentFlashcard.status === FlashcardStatus.GRADUATED) {
     if (quality === FlashcardQuality.REPEAT) {
       if (currentFlashcard.failed_consecutive_attempts !== 0) {
         if (currentFlashcard.ease_factor < 150) {
@@ -58,4 +66,5 @@ export const spacedRepetition = (
       currentFlashcard.failed_consecutive_attempts = 0;
     }
   }
+  currentFlashcard.learning_status = learning_status;
 };
