9807d04ac48cbbe0fa11b896c0407a572d3201e4
Jonathan van Wersch <jonathanvwersch@gmail.com>
2021-08-07 11:53:28 +0100

    Update environments

--- src/db/database.ts
+++ src/db/database.ts
@@ -2,8 +2,7 @@ import knex from "knex";
 const knexfile = require("../../knexfile.js");
 
 const env = process.env.NODE_ENV || "development";
-if (process.env.NODE_ENV === "development") {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-}
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const db = knex(knexfile[env]);
 export default db;
