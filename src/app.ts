7aa14a1b1114f19ba8963c1484caef2c464a5371
Jonathan van Wersch <jonathanvwersch@gmail.com>
2021-10-04 22:00:07 +0100

    allow localhost

--- src/app.ts
+++ src/app.ts
@@ -39,10 +39,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function (_, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
